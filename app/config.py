d7f72f99c642dd3e53cb344a74740623248974f1
a-h-a-m <ahasanmubarok@gmail.com>
2026-07-08 23:06:37 +0700

    Initial FastAPI project setup

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+GREEN_API_INSTANCE_ID = os.getenv(
+    "GREEN_API_INSTANCE_ID"
+)
+
+GREEN_API_TOKEN = os.getenv(
+    "GREEN_API_TOKEN"
+)
